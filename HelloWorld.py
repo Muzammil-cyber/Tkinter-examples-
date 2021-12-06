dca71d13acd0e124ad1b502daadfa9cc1b495680
Muzammil Loya <73660510+Muzammil-cyber@users.noreply.github.com>
2021-12-06 23:24:31 +0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+from tkinter import *
+# Creating a Main Window
+mainWindow = Tk()
+# Creating a Label(Hello World)
+Label1 = Label(mainWindow, text="Hello World")
+# Shoving the Label to the Main Window
+Label1.pack()
+mainWindow.mainloop()
